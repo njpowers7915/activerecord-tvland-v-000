9ca77c12ef5c240fc88aa054bf9def2591984825
Nicholas Powers <njpowers7915@gmail.com>
2018-05-31 13:31:31 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -3,8 +3,13 @@ class Actor < ActiveRecord::Base
   has_many :shows, through: :characters
   
   def full_name
-    #first_name = self.first_name
-    #last_name = self.last_name
     full_name = "#{self.first_name} #{self.last_name}"
   end
+  
+  def list_roles
+    self.characters.each do |i|
+      puts "#{i.name} - #{i.show.name}"
+    end
+  end
+    
 end
