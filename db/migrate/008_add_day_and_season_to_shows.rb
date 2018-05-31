4617e56a6688b2dc48f30c12dc6685f5f5dd9f6e
Nicholas Powers <njpowers7915@gmail.com>
2018-05-31 13:38:58 +0000

    automatically backed up by learn

--- db/migrate/008_add_day_and_season_to_shows.rb
+++ db/migrate/008_add_day_and_season_to_shows.rb
@@ -1,5 +1,6 @@
 class AddDayAndSeasonToShows < ActiveRecord::Migration[4.2]
   def change
+    add_column :shows, :catchphrase, :string
     add_column :characters, :catchphrase, :string
   end
 end
