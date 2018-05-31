dd53098f384c30f11c84e64580cca7ca772d6f85
Nicholas Powers <njpowers7915@gmail.com>
2018-05-31 13:13:55 +0000

    automatically backed up by learn

--- db/migrate/006_add_characters_to_actors.rb
+++ db/migrate/006_add_characters_to_actors.rb
@@ -1,4 +1,4 @@
-class AddShowsToNetwork < ActiveRecord::Migration[4.2]
+class AddCharactersToActors < ActiveRecord::Migration[4.2]
   def change
     add_column :characters, :actor_id, :integer
   end
