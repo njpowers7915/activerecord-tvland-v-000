608f81b5656c886cf4ffaee99c3a61e8553e7d47
Nicholas Powers <njpowers7915@gmail.com>
2018-05-31 13:26:41 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -2,7 +2,7 @@ class Actor < ActiveRecord::Base
   has_many :characters
   has_many :shows, through: :characters
   
-  def self.full_name
+  def full_name
     #first_name = self.first_name
     #last_name = self.last_name
     full_name = "#{self.first_name} #{self.last_name}"
